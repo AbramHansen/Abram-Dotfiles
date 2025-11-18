127a99b14bc51a6e7dc378a3191b2616a358337f
Abram Hansen <hansenabram@gmail.com>
2025-11-18 01:22:34 -0700

    greatly improved gropen function with batcat prieview

--- bash/gropen.sh
+++ bash/gropen.sh
@@ -5,10 +5,14 @@ function gropen {
         directory="."
     fi
 
-    local filename=$(grep -r "$search" "$directory" | fzf | cut -d: -f1)
+    preview_command='sh -c "batcat --theme=base16 --style=numbers --color=always --line-range $(($(echo {} | cut -d: -f2)-5)):$(($(echo {} | cut -d: -f2)+100)) --highlight-line $(echo {} | cut -d: -f2) $(echo {} | cut -d: -f1)"'
 
-    # open the file in neovim with the searched for text highlighted
-    if ! [[ -z $filename ]]; then
-        nvim -c "/$search" $filename
+    local selected_line=$(grep -iIRn "$search" "$directory" 2>/dev/null | fzf --preview "$preview_command")
+
+    # open the file in neovim with line selected
+    if ! [[ -z $selected_line ]]; then 
+        local filename=$(echo "$selected_line" | cut -d: -f1)
+        local line_number=$(echo "$selected_line" | cut -d: -f2)
+        nvim "+$line_number" "$filename" 
     fi
 }
